7fb3486fdb12877d7f6839eb887e88757906f480
Akinrinade Oluwaseun <akinrinadeseyi@gmail.com>
2022-09-13 06:02:39 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "_putchar.c"
+
+/**
+* main - prints _putchar
+*
+*  Description: prints _putchar
+*
+* Return: 0 (Success);
+*/
+int main(void)
+{
+char text[10] = "_putchar"
+int i = 0;
+for (i = 0; i < 9; i++)
+{
+_putcher(text[i])
+}
+_putchar('\n')
+return (0);
+}
