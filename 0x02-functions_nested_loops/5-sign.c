e08a1be2f4399e757eb5c7fd536a5e635b545153
Akinrinade Oluwaseun <akinrinadeseyi@gmail.com>
2022-09-14 03:13:48 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,13 +2,13 @@
 
 /**
 * print_sign - prints the sign of a number
+* @n: n is an integer
 *
 * Return: 1 (if positive), 0 (if zero), -1 (if negative)
 *
 */
 
 int print_sign(int n)
-
 {
 if (n > 0)
 {
