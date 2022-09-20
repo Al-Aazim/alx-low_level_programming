9caed6cb2772b9adec45bdd0d949634acaf52d57
Akinrinade Oluwaseun <akinrinadeseyi@gmail.com>
2022-09-20 01:42:57 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,14 +10,20 @@ void puts_half(char *str)
 {
 int i, n,len;
 
-i = 0;
-while (str[i] != '\0')
+len = 0;
+while (str[len] != '\0')
 {
-i++;
+len++;
 }
-len = i;
-n = len / 2;
-while (n<= len)
+if (len % 2 == 0)
+{
+for (i = len / 2; str[i] != '\n'; i++)
+{
+_putchar(str[i]);
+}
+else if (len % 2)
+{
+for (n = (len - 1) / 2; n < len- 1; n++)
 {
 _putchar(str[n]);
 n++;
