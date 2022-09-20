3f7b504d2e0f04a7f5b94ce233d25d7629a94260
Akinrinade Oluwaseun <akinrinadeseyi@gmail.com>
2022-09-20 07:51:45 -0700

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,17 +11,21 @@
 void print_array(int *a, int n)
 {
 int i, count;
+
 i = 0;
 count = 1;
 if (n < 0)
 n = 0;
 if (n > 0)
 {
+
 while (a[i] != '\0' && count < n)
 {
+
 printf("%d, ", a[i]);
 i++;
-count++;
+count++;i
+
 }
 printf("%d\n", a[i]);
 }
